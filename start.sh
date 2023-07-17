0246e88555439e55d3a0586e57a18ebcf21b380d
Sabastian Highton <70707483+shighton@users.noreply.github.com>
2023-07-17 11:26:03 -0400

    Update start.sh
    Test

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd /workspace
 echo "hello"
-#{ python main.py & } 2>/dev/null
-# { python main.py & } 2>/dev/null
-# For visdom
-# { python -m visdom.server & } 2>/dev/null
+{ python -m visdom.server & } 2>/dev/null
