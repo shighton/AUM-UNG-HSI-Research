a129b13c7f0eb180bc44502f0a9da25675113ade
Sabastian Highton <70707483+shighton@users.noreply.github.com>
2023-07-23 20:57:56 -0400

    Update and rename autoDev.bash to iter.sh
    Created an sh file

--- autoDev.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Use this to update the SIF file. Will replace outdated DeepHSI,
-# build, and copy the new SIF file to the bridges2 project folder.
-# Replace "shighton" with your information.
-cd ..
-rm -r DeepHSI/
-git clone https://github.com/shighton/DeepHSI.git && cd DeepHSI
-apptainer build new.sif test.def
-scp new.sif shighton@bridges2.psc.edu:/ocean/projects/cts090005p/shighton
---
+++ iter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin
+cd ..
+./autoDev.bash
